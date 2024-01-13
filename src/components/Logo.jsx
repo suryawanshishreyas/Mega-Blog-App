222c656613f7905708eadf4d49c2625cba7e463a
suryawanshishreyas <suryawanshishreyas@gmail.com>
2024-01-13 23:43:46 +0530

    minor bugs need fix

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+// eslint-disable-next-line react/prop-types, no-unused-vars
+function Logo({width = '100px'}) {
+  return (
+    <div>Logo</div>
+  )
+}
+
+export default Logo
